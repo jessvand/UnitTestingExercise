4c3161e212878de6e0b3cde4b316c8f5c5684b20
jessvand <jmvandbeek@gmail.com>
2024-06-02 18:07:50 -0400

    Got all 4 tests to run green

--- UnitTestingExercise.Tests/UnitTests.cs
+++ UnitTestingExercise.Tests/UnitTests.cs
@@ -13,54 +13,56 @@ namespace UnitTestingExercise.Tests
 
             //Arrange
             // create a Calculator object
-            
+            var calcuInstance = new Calculator();
 
             //Act
-                // call the Add method that is located in the Calculator class
-                // and store its result in a variable named actual
+            // call the Add method that is located in the Calculator class
+            // and store its result in a variable named actual
+
+            var act = calcuInstance.Add(num1, num2, num3);
 
             //Assert
-                //Assert.Equal(expected, actual);
+            Assert.Equal(expected, act);
         }
 
         [Theory]
-        [InlineData()]//Add test data <-------
+        [InlineData(10, 3, 7)]//Add test data <-------
+
         public void SubtractTest(int minuend, int subtrhend, int expected)
         {
             //Start Step 5 here:
-
             //Arrange
-
+            var calcuInstance = new Calculator();
             //Act
-
+            var act = calcuInstance.Subtract(minuend, subtrhend);
             //Assert
-
+            Assert.Equal(expected, act);
         }
 
         [Theory]
-        [InlineData()]//Add test data <-------
+        [InlineData(4,10,40)]//Add test data <-------
         public void MultiplyTest(int num1, int num2, int expected)
         {
             //Start Step 7 here:
 
             //Arrange
-
+            var calcuInstance = new Calculator();
             //Act
-
+            var act = calcuInstance.Multiply(num1, num2);
             //Assert
-
+            Assert.Equal(expected, act);
         }
 
         [Theory]
-        [InlineData()]//Add test data <-------
+        [InlineData(32, 4, 8)]//Add test data <-------
         public void DivideTest(int num1, int num2, int expected)
         {
             //Arrange
-
+            var calcuInstance =  new Calculator(); 
             //Act
-
+            var act = calcuInstance.Divide(num1, num2);
             //Assert
-
+            Assert.Equal(expected, act);
         }
 
     }
